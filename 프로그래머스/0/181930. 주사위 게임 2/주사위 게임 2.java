d4fe9386063ea008f03a637eab543ea5df1fe5f6
yumya <yumya47@gmail.com>
2025-01-14 11:30:46 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = a + b + c;
+        if (a == b || b == c || a == c) {
+            answer *= (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+        }
+        if (a == b && b == c) {
+            answer *= (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+        }
+        return answer;
+    }
+}
