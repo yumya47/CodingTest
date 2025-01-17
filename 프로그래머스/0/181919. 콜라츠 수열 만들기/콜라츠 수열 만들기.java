9748e547cef002e7fd23b28e27c8d9214d8c4cde
yumya <yumya47@gmail.com>
2025-01-17 12:03:31 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 14.70 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            answer.add(n);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
