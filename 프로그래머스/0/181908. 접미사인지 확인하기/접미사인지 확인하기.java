908d7050a21d63734c5f2bca5758c0ecf9acbe3c
yumya <yumya47@gmail.com>
2025-01-21 11:50:32 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.substring(i, my_string.length()).equals(is_suffix)) {
-                answer = 1;
-                break;
-            }
-        }
+        int answer = my_string.endsWith(is_suffix) ? 1 : 0;
         return answer;
     }
 }
