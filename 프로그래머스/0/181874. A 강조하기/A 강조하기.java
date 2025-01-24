0ead57788c7f98f40fa2c44077cbd2140c0a5b08
yumya <yumya47@gmail.com>
2025-01-24 11:08:54 +0900

    [level 0] Title: A 강조하기, Time: 7.89 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'a' || myString.charAt(i) == 'A') {
+                answer += "A";
+            } else {
+                answer += (myString.charAt(i) + "").toLowerCase();
+            }
+        }
+        return answer;
+    }
+}
