0a7e40abcade6bb887f52e22deb2bacac2b721e1
yumya <yumya47@gmail.com>
2025-02-10 23:53:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.16 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int temp = 0;
+        for(int i = 0; i < array.length - 1; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array[array.length / 2];
+    }
+}
