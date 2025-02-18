d4b3063745c77ab7caea7cba4ba021711edb499b
yumya <yumya47@gmail.com>
2025-02-18 22:56:18 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int a : arr) {
+            answer += a;
+        }
+        return answer / arr.length;
+    }
+}
